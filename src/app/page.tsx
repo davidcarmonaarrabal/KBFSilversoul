92f6dc36de287ecf799bfbacb722d663c300c323
David <davidcarmonaarrabal18@gmail.com>
2024-12-30 12:56:42 +0100

    Minor fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,8 +10,8 @@ export default function Home() {
   return (
     <div className={`${pixelifySans.className}`}>
       <header className="pt-10 flex flex-row justify-center">
-        <Image src="/logo.png" alt="logo" width={200} height={60}/>
-        <h1 className="text-4xl pt-6">
+        <Image src="/logo1.png" alt="logo" width={100} height={60}/>
+        <h1 className="text-4xl pt-6 pl-3">
           Mechanical Keyboards Fun{" "}
           <span className="text-2xl">by Silversoul07</span>
         </h1>
