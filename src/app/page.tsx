4b9898be5718d8a1dc195622f0641765e0ee6cca
David <davidcarmonaarrabal18@gmail.com>
2025-01-03 08:43:39 +0100

    Edited index

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,10 @@ const pixelifySans = Pixelify_Sans({
 
 export default function Home() {
   return (
-    <div className={`${pixelifySans.className}`}>
-      <header className="pt-10 flex flex-row justify-center space-x-96">
+    <div className={`${pixelifySans.className} flex flex-col items-center`}>
+      <header className="m-4 mt-10 pl-10 pr-10 flex flex-row justify-center space-x-96 bg-white border-black border-4 rounded-xl shadow-[5px_5px_0px_rgba(0,0,0,0.1)]">
         <div className="flex flex-row justify-center">
-          <Image src="/logo1.png" alt="logo" width={100} height={60}/>
+          <Image src="/logo1.png" alt="logo" width={100} height={60} />
           <h1 className="text-4xl pt-6 pl-3">
             Mechanical Keyboards Fun{" "}
             <span className="text-2xl">by Silversoul07</span>
@@ -23,6 +23,20 @@ export default function Home() {
           <button className="hover:text-blue-900 hover:border-b-2 hover:border-black transition duration-300">Sobre mí</button>
         </div>
       </header>
+
+      <main className="m-4 bg-white border-black border-4 rounded-xl shadow-[5px_5px_0px_rgba(0,0,0,0.1)] w-[1080px] h-[700px] p-0 flex justify-center items-center overflow-hidden relative">
+        <div className="absolute inset-0">
+          <Image
+            src="/keyboards.jpg"
+            alt="keyboards"
+            fill
+            className="object-cover rounded-xl"
+          />
+        </div>
+        <div className="absolute z-10 text-white bg-black bg-opacity-70 p-4 rounded">
+          <h1>As simple as beautiful and useful</h1>
+        </div>
+      </main>
     </div>
   );
 }
