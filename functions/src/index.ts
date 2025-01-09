b94dd6db28feec25df3bafb7de9167666f76d3d4
David <davidcarmonaarrabal18@gmail.com>
2025-01-09 13:09:51 +0100

    Deploying works

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,14 +2,11 @@ import * as functions from 'firebase-functions';
 import express from 'express';
 import sgMail from '@sendgrid/mail';
 
+// Accede a la clave de API de SendGrid desde las variables de entorno
+sgMail.setApiKey(process.env.SENDGRID_API_KEY!);
 
-// Configura SendGrid
-sgMail.setApiKey(functions.config().sendgrid.apikey);
-
-// Crea una instancia de Express
 const app = express();
 
-// Configura un endpoint POST para el formulario de contacto
 app.post('/send-email', async (req, res) => {
     const { name, email, message } = req.body;
 
@@ -27,12 +24,6 @@ app.post('/send-email', async (req, res) => {
         console.error(error);
         res.status(500).json({ error: 'Error al enviar el correo' });
     }
-});
-
-// Configura el puerto y permite que Express funcione con Firebase Functions
-const port = process.env.PORT || 8081;  // Cambiar 8081 a cualquier puerto libre  // Asegúrate de que está en el puerto 8080
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
 });
 
 // Exporta la función para que Firebase pueda ejecutarla
