e9ab1b667c8e3b79a8074c6bed885da2848426ae
David <davidcarmonaarrabal18@gmail.com>
2025-01-13 08:56:45 +0100

    Made the ModsMain works with a function instead of HTML code

--- src/components/ModsMain.tsx
+++ src/components/ModsMain.tsx
@@ -1,38 +1,33 @@
 import ModsDiv from "./ModDiv";
 
+type ModItem = {
+    url: string;
+    name: string;
+    link: string;
+};
+
+const modItems: ModItem[] = [
+    { url: "/img/KeyboardMini.png", name: "How does a Keyboard works?", link: "/mods/HDAKW/" },
+    { url: "/img/Tape.png", name: "Tape Mod", link: "/mods/Tape/" },
+    { url: "/img/Isolation.png", name: "Isolation", link: "/mods/Isolation/" },
+    { url: "/img/Stab.png", name: "Stabs Lube", link: "/mods/Stab_lube/" },
+    { url: "/img/Switchlube.png", name: "Switch lube", link: "/mods/Switch_lube/" },
+    { url: "/img/BiggerKeycap.png", name: "Keycaps", link: "/mods/Keycaps/" },
+    { url: "/img/Welding.png", name: "Welding", link: "/mods/Welding/" },
+    { url: "/img/Switches.png", name: "Types of Switches", link: "/mods/Types_Switches/" },
+    { url: "/img/Connections.png", name: "Types of Connections", link: "/mods/Connections/" },
+    { url: "/img/IsoAnsi.png", name: "ISO or ANSI", link: "/mods/ISO_ANSI/" },
+];
 
 const ModsMain: React.FC = () => {
+    const renderMods = () => 
+        modItems.map((item, index) => (
+            <ModsDiv key={index} url={item.url} name={item.name} link={item.link} />
+        ));
+
     return (
         <div className="flex flex-col mb-4 gap-4 md:flex-row md:flex-wrap md:justify-center md:items-center">
-            <ModsDiv url="/img/KeyboardMini.png" name="How does a Keyboard works?"
-                link="/mods/HDAKW/" />
-
-            <ModsDiv url="/img/Tape.png" name="Tape Mod"
-                link="/mods/Tape/" />
-
-            <ModsDiv url="/img/Isolation.png" name="Isolation"
-                link="/mods/Isolation/" />
-
-            <ModsDiv url="/img/Stab.png" name="Stabs Lube"
-                link="/mods/Stab_lube/" />
-
-            <ModsDiv url="/img/Switchlube.png" name="Switch lube"
-                link="/mods/Switch_lube/" />
-
-            <ModsDiv url="/img/BiggerKeycap.png" name="Keycaps"
-                link="/mods/Keycaps/" />
-
-            <ModsDiv url="/img/Welding.png" name="Welding"
-                link="/mods/Welding/" />
-
-            <ModsDiv url="/img/Switches.png" name="Types of Switches"
-                link="/mods/Types_Switches/" />
-
-            <ModsDiv url="/img/Connections.png" name="Types of Connections"
-                link="/mods/Connections/" />
-
-            <ModsDiv url="/img/IsoAnsi.png" name="ISO or ANSI"
-                link="/mods/ISO_ANSI/" />
+            {renderMods()}
         </div>
     );
 };
