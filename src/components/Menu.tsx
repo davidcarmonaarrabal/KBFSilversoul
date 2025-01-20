19aa1bc8bafdaf09896d310d30b91b5ee7f9d4d4
David <davidcarmonaarrabal18@gmail.com>
2025-01-20 09:41:58 +0100

    Changed aspect ratios and some functions

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
-
 import { usePathname } from "next/navigation";
+
 
 const Menu: React.FC = () => {
     const pathname = usePathname();
--- src/components/MiniCKeyboard.tsx
+++ src/components/MiniCKeyboard.tsx
@@ -5,22 +5,22 @@ import { MousePointerClick } from "lucide-react";
 import Image from "next/image";
 
 interface KeyboardData {
-  name: string;
-  switches: string;
-  base: string;
-  keycaps: string;
-  keycapsn: number;
-  descriptions: string;
-  descriptionl: string;
-  img: string;
-  web: string;
-  size: string;
-  region: string;
+    name: string;
+    switches: string;
+    base: string;
+    keycaps: string;
+    keycapsn: number;
+    descriptions: string;
+    descriptionl: string;
+    img: string;
+    web: string;
+    size: string;
+    region: string;
 }
 
 const MiniCKeyboard: React.FC = () => {
-    const [keyboardData, setKeyboardData] = useState<KeyboardData[]>([]); 
-    const [currentKeyboardIndex, setCurrentKeyboardIndex] = useState<number>(0); 
+    const [keyboardData, setKeyboardData] = useState<KeyboardData[]>([]);
+    const [currentKeyboardIndex, setCurrentKeyboardIndex] = useState<number>(0);
 
     useEffect(() => {
         fetch("https://sendemail-3fujvy5ywq-uc.a.run.app/keyboards")
@@ -36,13 +36,13 @@ const MiniCKeyboard: React.FC = () => {
             setCurrentKeyboardIndex((prevIndex) => {
                 return (prevIndex + 1) % keyboardData.length;
             });
-        }, 10000); 
+        }, 10000);
 
         return () => clearInterval(intervalId);
     }, [keyboardData]);
 
     if (keyboardData.length === 0) {
-        return <div>Loading...</div>;
+        return <div className="text-black text-2xl">Loading...</div>;
     }
 
     const currentKeyboard = keyboardData[currentKeyboardIndex];
@@ -51,11 +51,11 @@ const MiniCKeyboard: React.FC = () => {
         <div className='bg-white border-black border-4 rounded-xl shadow-[5px_5px_0px_rgba(0,0,0,0.1)] w-[345px] sm:w-[615px] 
         md:w-[740px] lg:w-[1000px] xl:w-[1240px] 2xl:w-[1500px] flex flex-col m-6 lg:flex-row text-black'>
             <div className="m-2 flex flex-col items-center justify-center">
-                <Image 
-                    src={currentKeyboard.img || '/img/maintenance.png'} 
-                    alt="Keyboard Image" 
-                    width={300} 
-                    height={300} 
+                <Image
+                    src={currentKeyboard.img || '/img/maintenance.png'}
+                    alt="Keyboard Image"
+                    width={300}
+                    height={300}
                     className="border-black border-2 rounded-md sm:w-[500px] sm:h-[500px]
                     md:w-[600px] md:h-[600px] lg:w-[500px] lg:h-[500px] 2xl:w-[650px]
                     2xl:h-[500px]"
@@ -63,7 +63,7 @@ const MiniCKeyboard: React.FC = () => {
             </div>
             <div className="m-2 items-center justify-center flex flex-col 2xl:ml-5 text-black">
                 <ul className="flex flex-col justify-center items-center lg:items-start lg:mt-8
-                xl:mt-10">
+                xl:mt-10 lg:w-[500px] xl:w-[590 px] 2xl:w-[950px]">
                     <li className="text-2xl xl:text-4xl">Name: {currentKeyboard.name}</li>
                     <li className="xl:text-2xl">Switches: {currentKeyboard.switches}</li>
                     <li className="xl:text-2xl">Base: {currentKeyboard.base}</li>
